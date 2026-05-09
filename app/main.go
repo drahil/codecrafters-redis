90f8267307e1d7edc74d1bbc677b48cb58a4dd07
Omar Djecevic <djecevic.omar@gmail.com>
2026-05-09 15:15:34 +0200

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -14,7 +14,7 @@ var _ = net.Listen
 var _ = os.Exit
 
 type Entry struct {
-	Value string
+	Value      string
 	ExpireTime int64
 }
 
@@ -47,15 +47,15 @@ func handleConnection(conn net.Conn) {
 
 	for {
 		args, err := getArgs(conn)
-		
+
 		if err != nil {
 			break
 		}
-		
+
 		if len(args) == 0 {
 			continue
 		}
-		
+
 		switch args[0] {
 		case "ping":
 			conn.Write([]byte("+PONG\r\n"))
@@ -69,8 +69,10 @@ func handleConnection(conn net.Conn) {
 			conn.Write([]byte(getValue(db[args[1]])))
 		case "rpush":
 			conn.Write([]byte(rpushValue(args, lists)))
+		case "lrange":
+			conn.Write([]byte(lrange(args, lists)))
 		}
-		
+			
 	}
 }
 
@@ -111,14 +113,14 @@ func parseMessage(message string) []string {
 	return args
 }
 
-func getArgs(conn net.Conn) ([]string, error){
+func getArgs(conn net.Conn) ([]string, error) {
 	buf := make([]byte, 1024)
 	n, err := conn.Read(buf)
 
 	raw := string(buf[:n])
 	args := parseMessage(raw)
 	fmt.Printf("%#v\n", args)
-	
+
 	return args, err
 }
 
@@ -136,52 +138,90 @@ func respInteger(raw int) string {
 	return fmt.Sprintf(":%s\r\n", stringRaw)
 }
 
+func respArray(values []string) string {
+	if len(values) == 0 {
+		return "*0\r\n"
+	}
+
+	var builder strings.Builder
+	builder.WriteString(fmt.Sprintf("*%d\r\n", len(values)))
+
+	for _, value := range values {
+		builder.WriteString(respEncoder(value))
+	}
+
+	return builder.String()
+}
+
 func setValue(args []string, db map[string]Entry) string {
 	var expireTime int64 = -1
 
-	if(len(args) > 3 && args[3] == "ex") {
+	if len(args) > 3 && args[3] == "ex" {
 		expireTime, _ = strconv.ParseInt(args[4], 10, 64)
 		expireTime *= 1000
 		nowMs := time.Now().UnixMilli()
 		expireTime = expireTime + nowMs
-	} 
-	if(len(args) > 4 && args[3] == "px"){
+	}
+	if len(args) > 4 && args[3] == "px" {
 		expireTime, _ = strconv.ParseInt(args[4], 10, 64)
 		nowMs := time.Now().UnixMilli()
 		expireTime = expireTime + nowMs
 	}
-	
+
 	db[args[1]] = Entry{Value: args[2], ExpireTime: expireTime}
 	return simpleEncoder("OK")
 }
 
 func getValue(entry Entry) string {
-	if (entry.Value == "") {
+	if entry.Value == "" {
 		return "$-1\r\n"
 	}
-	
-	if (entry.ExpireTime == -1) {
-		return  respEncoder(entry.Value)
+
+	if entry.ExpireTime == -1 {
+		return respEncoder(entry.Value)
 	}
-	
+
 	nowMs := time.Now().UnixMilli()
-	
-	if(nowMs <= entry.ExpireTime) {
-		return  respEncoder(entry.Value)
+
+	if nowMs <= entry.ExpireTime {
+		return respEncoder(entry.Value)
 	}
-	
+
 	return "$-1\r\n"
 }
 
 func rpushValue(args []string, lists map[string][]string) string {
 	listName := args[1]
 	values := args[2:]
-	
+
 	if existingList, ok := lists[listName]; ok {
 		lists[listName] = append(existingList, values...)
 	} else {
 		lists[listName] = values
 	}
-	
+
 	return respInteger(len(lists[listName]))
+}
+
+func lrange(args []string, lists map[string][]string) string {
+	listName := args[1]
+	startingIndex, _ := strconv.ParseInt(args[2], 10, 64)
+	endingIndex, _ := strconv.ParseInt(args[3], 10, 64)
+	
+	if existingList, ok := lists[listName]; ok {
+		if startingIndex >= int64(len(existingList)) {
+			return respArray([]string{})
+		}
+		
+		if endingIndex >= int64(len(existingList)) {
+			return respArray(existingList[startingIndex:len(existingList)-1])
+		}
+		
+		if startingIndex > endingIndex {
+			return respArray([]string{})
+		}
+		return respArray(existingList[startingIndex:endingIndex])
+	}
+	
+	return respArray([]string{})
 }
