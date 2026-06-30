b3fbf945acd2c5d1bf73ed84e06a4e41e132a191
Omar Djecevic <djecevic.omar@gmail.com>
2026-06-30 21:09:34 +0200

    codecrafters submit [skip ci]

--- internal/command/replications.go
+++ internal/command/replications.go
@@ -3,7 +3,7 @@ package command
 import "github.com/codecrafters-io/redis-starter-go/internal/resp"
 
 func (h *Handler) info(client *ClientState, args []string) string {
-	if args[2] == "replication" {
+	if args[1] == "replication" {
 		return resp.BulkString("role:master")
 	}
 
